ad31d7570c3227650ee748026c3782371b7976b8
Temirlan Ragimov <temirlan.official@mail.ru>
2025-06-14 21:22:34 +0300

    limiter добавил

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"net"
+	"time"
 
 	"PWZ1.0/internal/app/order"
 	"PWZ1.0/internal/mw"
 	"PWZ1.0/internal/service"
 	"PWZ1.0/internal/storage"
 	desc "PWZ1.0/pkg/pwz"
+	"github.com/ulule/limiter/v3"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -23,7 +26,7 @@ func main() {
 	orderService := service.NewOrderService(storage)
 	orderServer := order.NewHandler(orderService)
 
-	grpcServer := grpc.NewServer(grpc.ChainUnaryInterceptor(mw.ValidateInterceptor))
+	grpcServer := grpc.NewServer(grpc.ChainUnaryInterceptor(mw.ValidateInterceptor, mw.RateLimiterInterceptor(limiter.New(memory.NewStore(), limiter.Rate{Period: 10 * time.Second, Limit: 2}))))
 	reflection.Register(grpcServer)
 	desc.RegisterNotifierServer(grpcServer, orderServer)
 
--- go.mod
+++ go.mod
@@ -7,11 +7,13 @@ toolchain go1.23.10
 require (
 	github.com/envoyproxy/protoc-gen-validate v1.2.1
 	github.com/google/uuid v1.6.0
+	github.com/ulule/limiter/v3 v3.11.2
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
